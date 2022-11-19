98a16e4808f4c6d96e31e610068a69d411958564
davey-mwaura <david.mwaura@student.moringaschool.com>
2022-11-19 19:41:44 +0300

    Completed assignment

---
+++ objects.js
@@ -0,0 +1,10 @@
+const employee = {
+    name:"",
+    streetAddress: "",
+
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){};
+function destructivelyUpdateEmployeeWithKeyAndValue(key, value){};
+function deleteFromEmployeeByKey(employee, key){};
+function destructivelyDeleteFromEmployeeByKey(key, value){};
