15694c5de4cff5339250762b25499e8cb17de7fa
davey-mwaura <david.mwaura@student.moringaschool.com>
2022-11-22 11:42:01 +0300

    Completed assignment

---
+++ controlflow.js
@@ -0,0 +1,12 @@
+function scuberGreetingForFeet(distance){
+    if (distance < 100)
+    {console.log('good morning,we will be there shortly')}
+
+    else if (distance < 200)
+    {console.log('good morning')}
+
+    else (distance =>200)
+    {console.log('good morning,buckle in for a fun ride')}
+}
+
+scuberGreetingForFeet(200)
